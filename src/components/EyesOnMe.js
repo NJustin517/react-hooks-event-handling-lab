a0476d73bf4f5abecc25ecbffea81f2fd9c92fc8
Justin Luciano <justinlluciano@gmail.com>
2022-01-12 09:27:33 -0800

    commit tree 636c5ae18289bae39a3b07c0ed17ea3330b0ced1

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
-// Code EyesOnMe Component Here
+function EyesOnMe() {
+  function handleEyesOn() {
+    console.log("Good!");
+  }
+
+  function handleEyesOff() {
+    console.log("Hey! Eyes on me!");
+  }
+  return (
+    <button onFocus={handleEyesOn} onBlur={handleEyesOff}>
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
